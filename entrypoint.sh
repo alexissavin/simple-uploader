5a5284aa9a179c957ef9b3f720c5405beb64a830
alexissavin <alexis.savin@gmail.com>
2022-02-16 17:34:38 +0100

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-chown -R goapp:goapp /etc/simple_uploader/tokens
-chmod -R 750 /etc/simple_uploader/tokens
+mkdir -p /tmp/simple_uploader
+cp -r /etc/simple_uploader/tokens /tmp/simple_uploader/tokens
+chown -R goapp:goapp /tmp/simple_uploader/tokens
+chmod -R 750 /tmp/simple_uploader/tokens
 mkdir -p /var/html/simple_uploader/data
 chown -R goapp:goapp /var/html/simple_uploader/data
 chmod -R 770 /var/html/simple_uploader
 
-exec runuser -u goapp -- '/usr/local/bin/app' '$@'
+exec runuser -u goapp -- '/usr/local/bin/app -tokens /tmp/simple_uploader/tokens' '$@'
