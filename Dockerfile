814d48a5f69a86d9c73a0d757a2c62707435b61e
alexissavin <alexis.savin@gmail.com>
2022-02-16 16:15:15 +0100

    iNow handling default token file and data directory

--- Dockerfile
+++ Dockerfile
@@ -16,6 +16,19 @@ COPY . /go/src/app
 RUN GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o /go/bin/app
 
 FROM debian:bullseye-slim AS runtime-env
+
+RUN groupadd -g 999 goapp
+RUN useradd -r -u 999 -g goapp goapp
+
+RUN mkdir -p /etc/simple_uploader/tokens
+RUN chown goapp:goapp /etc/simple_uploader/tokens
+RUN chmod 750 /etc/simple_uploader/tokens
+RUN mkdir -p /var/html/simple_uploader/data
+RUN chown goapp:goapp /var/html/simple_uploader/data
+RUN chmod 770 /var/html/simple_uploader/data
+
 COPY --from=build-env /go/bin/app /usr/local/bin/app
+
+USER appuser
 EXPOSE 8080/tcp
 ENTRYPOINT ["/usr/local/bin/app"]
--- simple_uploader.go
+++ simple_uploader.go
@@ -16,25 +16,37 @@ func run(args []string) int {
 	tlsListenPort := flag.Int("tlsport", 8443, "port number to listen on with TLS")
 	// 5,242,880 bytes == 5 MiB
 	maxUploadSize := flag.Int64("upload_limit", 5242880, "max size of uploaded file (byte)")
-	tokensFlag := flag.String("tokens", "", "specify the file containing the security tokens")
+	tokensFlag := flag.String("tokens", "/etc/simple_uploader/tokens", "specify the file containing the security tokens")
 	maxattempts := flag.Int64("maxattempts", 3, "specify the maximum failed connection attempts")
 	logLevelFlag := flag.String("loglevel", "info", "logging level")
 	certFile := flag.String("cert", "", "path to certificate file")
 	keyFile := flag.String("key", "", "path to key file")
 	corsEnabled := flag.Bool("cors", false, "if true, add ACAO header to support CORS")
+	helpRequested := flag.Bool("help", false, "display the usage")
 	flag.Parse()
-	serverRoot := flag.Arg(0)
-	if len(serverRoot) == 0 {
+
+	if *helpRequested == true {
 		flag.Usage()
 		return 2
 	}
+
+	serverRoot := flag.Arg(0)
+	if len(serverRoot) == 0 {
+		serverRoot = "/var/html/simple_uploader/data"
+
+		if _, err := os.Stat(serverRoot); errors.Is(err, os.ErrNotExist) {
+			logger.Fatal("Missing Data Directory")
+			return 1
+		}
+	}
+
 	if logLevel, err := logrus.ParseLevel(*logLevelFlag); err != nil {
 		logrus.WithError(err).Error("failed to parse logging level, so set to default")
 	} else {
 		logger.Level = logLevel
 	}
-	tokensFile := *tokensFlag
 
+	tokensFile := *tokensFlag
 	if tokensFile == "" {
 		logger.Fatal("Missing Tokens File")
 		return 1
