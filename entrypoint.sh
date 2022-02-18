f7c129426dccb21fba83f4813410bdc416224416
alexissavin <alexis.savin@gmail.com>
2022-02-18 09:09:33 +0100

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,9 @@ if [ -d '/etc/simple_uploader' ] && [ -d '/etc/simple_uploader/tokens' ]; then
   cp -r /etc/simple_uploader/tokens /tmp/simple_uploader/
   chown -R goapp:goapp /tmp/simple_uploader
   chmod -R 750 /tmp/simple_uploader/tokens
-  chmod 640 /tmp/simple_uploader/tokens/*
+  if [ `ls -1 '/tmp/simple_uploader/tokens'| wc -l` -gt 0 ]; then
+    chmod 640 /tmp/simple_uploader/tokens/*
+  fi
 fi
 
 exec runuser -u goapp "/usr/local/bin/app" -- "-tokens" "/tmp/simple_uploader/tokens/tokens" "$@"
