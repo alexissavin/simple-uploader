9cd1f9a90f490738267c05f602ede8776cbda711
alexissavin <alexis.savin@gmail.com>
2024-12-16 16:11:29 +0100

    Updating Curl Tests

--- test/upload.php
+++ test/upload.php
@@ -2,9 +2,7 @@
 
 function data_collection_send($url)
 {
-
-  $ts = microtime(TRUE);
-  $files = glob('./*.tgz');
+  $files = glob('*.tgz');
 
   if (is_array($files) && count($files)) {
     $ch = curl_init();
@@ -21,15 +19,19 @@ function data_collection_send($url)
       curl_setopt($ch, CURLOPT_POSTFIELDS, ['file' => $file]);
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
       curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
-      curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+      curl_setopt($ch, CURLOPT_TIMEOUT, 60);
       curl_setopt($ch, CURLOPT_DNS_SHUFFLE_ADDRESSES, TRUE);
       curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, TRUE);
       curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
       curl_setopt($ch, CURLOPT_ENCODING, 'gzip,deflate');
 
-      if (!empty($server['proxy']))
-        echo("Data Collection: Using proxy [127.0.0.1:8888]\n");
-        curl_setopt($ch, CURLOPT_PROXY, "127.0.0.1:8888");
+      curl_setopt($ch, CURLOPT_TCP_KEEPALIVE, 1);
+      curl_setopt($ch, CURLOPT_TCP_KEEPINTVL, 8);
+      curl_setopt($ch, CURLOPT_MAX_SEND_SPEED_LARGE, 512 * 1024);
+
+      //if (!empty($server['proxy']))
+      //  echo("Data Collection: Using proxy [127.0.0.1:8888]\n");
+      //  curl_setopt($ch, CURLOPT_PROXY, "127.0.0.1:8888");
 
       if (($res = curl_exec($ch)) === FALSE) {
         echo('Data Collection: Failed to upload file ['.$f.'] ('.curl_errno($ch).'): '.curl_error($ch)."\n");
@@ -59,6 +61,7 @@ function data_collection_send($url)
 
 if ($argc == 2) {
   echo("Data Collection: Target URL [" . $argv[1] . "]\n");
+  echo("Data Collection: PID [" . getmypid() . "]\n");
   data_collection_send($argv[1]);
 } else {
   echo("Please specify some URL and token such as: https://upload.test.com/upload?token=02c8797e-981e-11ee-82f6-674e065a753c\n");
